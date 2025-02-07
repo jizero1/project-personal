140ad7e212f5e186380ce805cd23a7e36f796115
jizero4101@naver.com <jizero4101@naver.com>
2025-02-07 18:24:27 +0900

    리포지토리 생성 후, 첫 커밋

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './App.css';
+import Main from './pages/Main/Main';
+function App() {
+
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Main</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Main/>}/>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
