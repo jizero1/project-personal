c9ef29bfc288457ee77b7e901728acc859752791
jizero4101@naver.com <jizero4101@naver.com>
2025-02-10 21:42:41 +0900

    main 디자인 전체 수정

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,39 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Main.css';
 import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
-import Personal from '../Personal/Personal';
+import { motion } from "framer-motion";
 
-const Main = () => {
+const MainTop = () => {
     const navigate = useNavigate(); // useNavigate()훅을 사용하여 페이지 이동
 
     const mainTestBtnClick = () => {
         navigate('/Personal');
     }
-    const mainCircleClick = () => {
-        navigate('/About');
-    }
     return (
-        <div className="main common-flex">
+        <div className="mainTop common-flex">
+            <div className="mainBoxAni"></div>
+            <div className="mainBoxAni"></div>
+            <div className="mainBoxAni"></div>
+            <div className="mainBoxAni"></div>
             <div className="main-test-container common-flex">
+
                 <p className="main-test-text">What's your Personal Color?</p>
                 <button className="main-test-Btn" onClick={mainTestBtnClick}>Personal Test</button>
             </div>
-            <div className="main-circle-outContainer common-flex">
-            <div className="main-circle-innerContainer">
-                <div className="main-circle main-circle-leftup common-flex" onClick={mainCircleClick}>
-                    <p className="main-circle-text">봄</p>
-                </div>
-                <div className="main-circle main-circle-leftdown common-flex" onClick={mainCircleClick}>
-                    <p className="main-circle-text">가을</p>
-                </div>
-                <div className="main-circle main-circle-rightup common-flex" onClick={mainCircleClick}>
-                    <p className="main-circle-text">여름</p>
-                </div>
-                <div className="main-circle main-circle-rightdown common-flex" onClick={mainCircleClick}>
-                    <p className="main-circle-text">겨울</p>
-                </div>
-            </div>
-            </div>
+        </div>
+    )
+}
+const MainBottom = () => {
+    return (
+        <div className="mainBottom">
+
+        </div>
+    )
+}
+const Main = () => {
+
+    return (
+        <div>
+            <MainTop></MainTop>
+            <MainBottom></MainBottom>
         </div>
     )
 }
