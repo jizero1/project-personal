a6c595950082b34b20609edb683a4ee98b2461a0
jizero4101@naver.com <jizero4101@naver.com>
2025-02-10 18:34:21 +0900

    main css 2차 수정 완료

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -9,25 +9,30 @@ const Main = () => {
     const mainTestBtnClick = () => {
         navigate('/Personal');
     }
+    const mainCircleClick = () => {
+        navigate('/About');
+    }
     return (
         <div className="main common-flex">
             <div className="main-test-container common-flex">
                 <p className="main-test-text">What's your Personal Color?</p>
                 <button className="main-test-Btn" onClick={mainTestBtnClick}>Personal Test</button>
             </div>
-            <div className="main-circle-container">
-                <div className="main-circle main-circle-leftup common-flex">
+            <div className="main-circle-outContainer common-flex">
+            <div className="main-circle-innerContainer">
+                <div className="main-circle main-circle-leftup common-flex" onClick={mainCircleClick}>
                     <p className="main-circle-text">봄</p>
                 </div>
-                <div className="main-circle main-circle-leftdown common-flex">
+                <div className="main-circle main-circle-leftdown common-flex" onClick={mainCircleClick}>
                     <p className="main-circle-text">가을</p>
                 </div>
-                <div className="main-circle main-circle-rightup common-flex">
+                <div className="main-circle main-circle-rightup common-flex" onClick={mainCircleClick}>
                     <p className="main-circle-text">여름</p>
                 </div>
-                <div className="main-circle main-circle-rightdown common-flex">
+                <div className="main-circle main-circle-rightdown common-flex" onClick={mainCircleClick}>
                     <p className="main-circle-text">겨울</p>
                 </div>
+            </div>
             </div>
         </div>
     )
