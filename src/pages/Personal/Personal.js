3f0fabba9c72e4316e6ad2f3e4b06437e487f597
jizero4101@naver.com <jizero4101@naver.com>
2025-02-22 01:00:16 +0900

    personal Test 작업중

--- src/pages/Personal/Personal.js
+++ src/pages/Personal/Personal.js
@@ -3,8 +3,10 @@ import './Personal.css';
 
 const Personal = () => {
     return (
-        <div>
-            Personal
+        <div className="personal-mainContainer">
+            <div>
+                <h2 className="personal-title-h2">Personal Test</h2>
+            </div>
         </div>
     )
 }
