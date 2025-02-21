fa27b6c693eb9bab539f5d54a9a79ccab88f8ce7
jizero4101@naver.com <jizero4101@naver.com>
2025-02-21 18:17:33 +0900

    메인페이지 텍스트 디자인 수정 및 화살표 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from './pages/Main/Main';
 import About from './pages/About/About';
 import Personal from './pages/Personal/Personal';
 import { motion } from "framer-motion";
+// import { FaAngleDown } from "react-icons/fa";
 
 function App() {
 
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import './Main.css';
 import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
 import { motion } from "framer-motion";
+import { FaAngleDown } from "react-icons/fa";
 
 const MainPage = () => {
     const navigate = useNavigate(); // useNavigate()훅을 사용하여 페이지 이동
@@ -12,6 +13,12 @@ const MainPage = () => {
 
     return (
         <div className="mainPage-container">
+            <div className="mainPage-text-container">
+                <h1 className="mainPage-text-h1">What's your Personal Color?</h1>
+                <p className="mainPage-text-p">나를 표현하는 퍼스널 컬러가 궁금하다면, 지금 바로 테스트 해보세요!</p>
+                <FaAngleDown className="mainPage-text-icon"/>
+                <button className="mainPage-text-btn">Personal Test</button>
+            </div>
         </div>
     )
 }
