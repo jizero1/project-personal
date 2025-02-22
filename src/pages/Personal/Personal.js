6720f71405167cedb0483a85ebf7c15aa610172d
jizero4101@naver.com <jizero4101@naver.com>
2025-02-22 21:30:57 +0900

    personal Test부분 UI디자인완료

--- src/pages/Personal/Personal.js
+++ src/pages/Personal/Personal.js
@@ -4,8 +4,16 @@ import './Personal.css';
 const Personal = () => {
     return (
         <div className="personal-mainContainer">
-            <div>
-                <h2 className="personal-title-h2">Personal Test</h2>
+            <div className="personal-test-container">
+                <p className="personal-test-question">질문 1. 당신은 어떤색상의 액세서리가 더 잘 어울리나요?</p>
+                <div className="personal-test-option">
+                    <div className="personal-test">
+                        <p>골드</p>
+                    </div>
+                    <div className="personal-test">
+                        <p>실버</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
