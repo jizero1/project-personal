cf4d25d550cbbd63b82e7054e8f1a6d5fc3e6fcf
jizero4101@naver.com <jizero4101@naver.com>
2025-02-24 00:21:30 +0900

    퍼스널테스트 질문지 작성중

--- src/pages/Personal/Personal.js
+++ src/pages/Personal/Personal.js
@@ -1,20 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Personal.css';
 
 const Personal = () => {
+    const [testStart, setTestStart] = useState(false);
+    const personalTestStart = () => {
+        setTestStart(!testStart);
+        console.log(testStart);
+    }
+
+    const [testNumber, setTestNumber] = useState(0);
+    const test = [
+        {
+            question: '질문 1. 당신에게 잘 어울리는 액세서리 색상은?',
+            option1: '골드',
+            option2: '실버',
+        },
+        {
+            question: '질문 2. ~~~',
+            option1: '옵션1',
+            option2: '옵션2',
+        },
+    ];
+    const nextTest = () => {
+        setTestNumber(testNumber + 1);
+        if (testNumber === 8) {
+            // 결과알려주는 창으로 이동하는 코드
+        }
+    }
     return (
         <div className="personal-mainContainer">
-            <div className="personal-test-container">
-                <p className="personal-test-question">질문 1. 당신은 어떤색상의 액세서리가 더 잘 어울리나요?</p>
-                <div className="personal-test-option">
-                    <div className="personal-test">
-                        <p>골드</p>
-                    </div>
-                    <div className="personal-test">
-                        <p>실버</p>
+            {testStart === false &&
+                <div className="personal-test-container">
+                    <p>Personal Test</p>
+                    <p>하나의 질문당 2가지의 선택지가 주어집니다.</p>
+                    <p>선택을 하면 다음 질문으로 넘어갑니다.</p>
+                    <p>총 8개의 질문에 선택을 완료하면 결과나 나타납니다.</p>
+                    <button onClick={personalTestStart}>시작하기</button>
+                </div>
+            }
+            {testStart &&
+                <div className="personal-test-container">
+                    <p className="personal-test-question">{test[testNumber].question}</p>
+                    <div className="personal-test-option">
+                        <div className="personal-test" onClick={nextTest}>
+                            <p>{test[testNumber].option1}</p>
+                        </div>
+                        <div className="personal-test" onClick={nextTest}>
+                            <p>{test[testNumber].option2}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            }
         </div>
     )
 }
